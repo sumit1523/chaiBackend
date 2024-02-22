0ca3b86535ad3e3a3bde94196b64c49efbe6c3b7
sumit kumar <sumit1523@gmail.com>
2024-02-23 00:55:16 +0530

    get request

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/twitter',(req,res)=>{
+    res.send("twitter")
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
